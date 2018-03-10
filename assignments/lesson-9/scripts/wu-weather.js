daf914d677201b40eb9ab2bd1508f034763f79cb
Jeff Ledbetter <jeffledbetter@Jeffs-MacBook-Pro.local>
2018-03-10 12:35:55 -0600

    u
    u

--- assignments/lesson-9/scripts/wu-weather.js
+++ assignments/lesson-9/scripts/wu-weather.js
@@ -1,49 +1,37 @@
 
 var header = document.querySelector('header');
 var section = document.querySelector('section');
-var requestURL = 'favPlayers.json';
+var requestURL = 'scripts/sanFran.json';
+
+/*
+var requestURL = 'http://api.wunderground.com/api/a4a4356fb4e85dd7/conditions/q/MN/Franklin.json';
+*/
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'text';
 request.send();
 request.onload = function() {
-    var superHeroesText = request.response;
-    var superHeroes = JSON.parse(superHeroesText);
-    populateHeader(superHeroes);
-    showHeroes(superHeroes);
+    var weatherDataText = request.response;
+    var weatherData = JSON.parse(weatherDataText);
+    showTownData(weatherData);
 }
-function populateHeader(jsonObj) {
-    var myH1 = document.createElement('h1');
-    myH1.textContent = jsonObj['squadName'];
-    header.appendChild(myH1);
-    var myPara = document.createElement('p');
-    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
-    header.appendChild(myPara);
-}
-function showHeroes(jsonObj) {
-    var heroes = jsonObj['members'];
-    for(var i = 0; i < heroes.length; i++) {
+function showTownData(jsonObj) {
+    var towns = jsonObj['members'];
+    for(var i = 0; i < 4; i++) {
         var myArticle = document.createElement('article');
         var myH2 = document.createElement('h2');
         var myPara1 = document.createElement('p');
         var myPara2 = document.createElement('p');
         var myPara3 = document.createElement('p');
-        var myList = document.createElement('ul');
-        myH2.textContent = heroes[i].name;
-        myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-        myPara2.textContent = 'Age: ' + heroes[i].age;
-        myPara3.textContent = 'Superpowers:';
-        var superPowers = heroes[i].powers;
-        for(var j = 0; j < superPowers.length; j++) {
-            var listItem = document.createElement('li');
-            listItem.textContent = superPowers[j];
-            myList.appendChild(listItem);
-        }
+        myH2.textContent = towns[i].display_location.full;
+        myPara1.textContent = 'Current temp: ' + towns[i].temp_f;
+        myPara2.textContent = 'Wind: ' + towns[i].wind_string;
+        myPara3.textContent = 'Feel like: ' + towns[i].feelslike_string;
+
         myArticle.appendChild(myH2);
         myArticle.appendChild(myPara1);
         myArticle.appendChild(myPara2);
         myArticle.appendChild(myPara3);
-        myArticle.appendChild(myList);
         section.appendChild(myArticle);
     }
 }
