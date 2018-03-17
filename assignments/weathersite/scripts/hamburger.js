2243ec4da8221784271895618b749460e1f7c939
jeffledbetter <led10003@gmail.com>
2018-03-17 17:12:15 -0500

    major edits
    added new images for individual sites. updated responsive sizing.

--- assignments/weathersite/scripts/hamburger.js
+++ assignments/weathersite/scripts/hamburger.js
@@ -2,8 +2,8 @@ function toggleNavMenu() {
     document.getElementById("toggleNav").classList.toggle("hide");
 }
 
-$(document).ready(function(){
-    $('#toggleNav').click(function(){
-        $(this).toggleClass('open');
-    });
-});
+// $(document).ready(function(){
+//     $('#toggleNav').click(function(){
+//         $(this).toggleClass('open');
+//     });
+// });
