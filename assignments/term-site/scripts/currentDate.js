4f4fc90bcc93435526a15fd2e4e94c176f1c1415
jeffledbetter <led10003@gmail.com>
2018-04-09 21:26:02 -0500

    fixed current date
    madd current date js work again

--- assignments/term-site/scripts/currentDate.js
+++ assignments/term-site/scripts/currentDate.js
@@ -1,10 +1,2 @@
-var cWeatherObject = new XMLHttpRequest();
-cWeatherObject.open('GET','http://api.wunderground.com/api/a4a4356fb4e85dd7/conditions/q/MN/Franklin.json',true);
-cWeatherObject.send();
-cWeatherObject.onload = function () {
-
-    var cWeatherInfo = JSON.parse(cWeatherObject.responseText);
-    console.log(cWeatherInfo);
-
-     document.getElementById('fCurrentDate').innerHTML = cWeatherInfo.current_observation.local_time_rfc822;
-};
+var d = new Date() ;
+document.getElementById("currentDate").innerHTML = d;
