5a7e7990c292f31273e6fd903311b1881cb965de
jeffledbetter <led10003@gmail.com>
2018-04-13 18:50:10 -0500

    ref page
    added.

--- assignments/term-site/scripts/slider.js
+++ assignments/term-site/scripts/slider.js
@@ -19,7 +19,7 @@ function showSlides(g) {
     if (g > slides.length) {slideIndex = 1}
 
     if (g < 1) {slideIndex = slides.length}
-    
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
