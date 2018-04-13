e7589af57aeed8daee312bb2740833148a298298
jeffledbetter <led10003@gmail.com>
2018-04-12 21:20:24 -0500

    update
    update

--- assignments/term-site/scripts/slider.js
+++ assignments/term-site/scripts/slider.js
@@ -11,17 +11,23 @@ function activeSlide(g) {
 }
 
 function showSlides(g) {
+
     var i;
     var slides = document.getElementsByClassName("salesSlides");
     var dots = document.getElementsByClassName("dot");
+
     if (g > slides.length) {slideIndex = 1}
+
     if (g < 1) {slideIndex = slides.length}
+    
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 }
