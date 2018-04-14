95d814b9c9f11ff2319a2a4bc9c5968f01797810
jeffledbetter <led10003@gmail.com>
2018-04-13 20:59:21 -0500

    fix
    was having issue with hamburger button not working on small view.

--- assignments/term-site/scripts/hamburger.js
+++ assignments/term-site/scripts/hamburger.js
@@ -2,6 +2,7 @@ function toggleNavMenu() {
     document.getElementById("toggleNav").classList.toggle("hide");
 }
 
+
 // $(document).ready(function(){
 //     $('#toggleNav').click(function(){
 //         $(this).toggleClass('open');
